074cb89884c819bbc420d33dab7429f07c97b6da
The Mai <mai.the.09@gmail.com>
2020-04-24 12:10:26 -0500

    Add test message for production

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import { GET_POST_ALL } from './queries/posts';
 
 function App() {
   return (
-    <div className="App">
-      This is your app
-    </div>
+    < div className="App" >
+      {console.log("GET_POST_ALL", useQuery(GET_POST_ALL))}
+    This is your app
+    </div >
   )
 }
 
